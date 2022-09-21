86014dd4dd27c8251be10c0fd3a083c9fe95e100
Pranjali <Pandrekarpranju@gmail.com>
2022-09-21 19:51:52 +0530

    This is my first commit

---
+++ Selenium_2022_Project/src/main/java/RadioButtonsandCheckBox.java
@@ -0,0 +1,60 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class RadioButtonsandCheckBox {
+	
+	static WebDriver driver;
+	public static void main(String[] args) {
+	
+
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+		
+		driver.get("http://demo.guru99.com/test/radio.html");	
+		driver.manage().window().maximize();
+		
+		WebElement radio1 = driver.findElement(By.id("vfb-7-1"));							
+		WebElement radio2 = driver.findElement(By.id("vfb-7-2"));							
+//
+//		//Radio Button1 is selected		
+		radio1.click();			
+		System.out.println("Radio Button Option 1 Selected");					
+//
+//		//Radio Button1 is de-selected and Radio Button2 is selected		
+		radio2.click();			
+		System.out.println("Radio Button Option 2 Selected");					
+//
+//		// Selecting CheckBox		
+		WebElement option1 = driver.findElement(By.id("vfb-6-0"));							
+//
+//		// This will Toggle the Check box 		
+		option1.click();			
+//
+//		// Check whether the Check box is toggled on 		
+		if (option1.isSelected()) {					
+			System.out.println("Checkbox is Toggled On");					
+
+		} else {			
+			System.out.println("Checkbox is Toggled Off");					
+		}		
+
+//
+//
+//		//Selecting Checkbox and using isSelected Method		
+		driver.get("http://demo.guru99.com/test/facebook.html");					
+		WebElement chkFBPersist = driver.findElement(By.id("persist_box"));		
+//		chkFBPersist.click (); 
+		for (int i=0; i<2; i++) {											
+			chkFBPersist.click (); 			
+			System.out.println("Facebook Persists Checkbox Status is -  "+chkFBPersist.isSelected());							
+		}		
+		//driver.close();		
+
+	}		
+
+}
+
